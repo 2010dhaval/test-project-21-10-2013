84081806a1abe0f568bad6ad419214f28c1431e2
dhaval.mehta@creativevirtual.com <dhaval.mehta@creativevirtual.com@68a613f8-3bbb-5ddf-70fa-e19565136997>
2013-10-23 11:17:41 +0000

    answer related changes...

--- TestProject/src/com/cv/service/AnswerService.java
+++ TestProject/src/com/cv/service/AnswerService.java
@@ -1,5 +1,14 @@
 package com.cv.service;
 
+import java.util.List;
+
+import com.cv.vo.AnswerVO;
+
 public interface AnswerService {
+	
+		
+	public void addOrEditAnswer(AnswerVO answerVO);
+	
+	public List<AnswerVO> getAnswerVoList();
 
 }
--- TestProject/src/com/cv/serviceImpl/AnswerServiceImpl.java
+++ TestProject/src/com/cv/serviceImpl/AnswerServiceImpl.java
@@ -1,9 +1,38 @@
 package com.cv.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cv.dao.AnswerDao;
+import com.cv.model.Answer;
 import com.cv.service.AnswerService;
+import com.cv.utils.DozerHelper;
+import com.cv.utils.DozerUtil;
+import com.cv.vo.AnswerVO;
+
 @Service
 public class AnswerServiceImpl implements AnswerService{
+	
+	@Autowired
+	private AnswerDao answerDao;	
+
+	@Override
+	public List<AnswerVO> getAnswerVoList() {
+		
+		List<AnswerVO> answerVOs = DozerHelper.map(DozerUtil.xmlConfig(), 
+				this.answerDao.getAnswerModelList(), AnswerVO.class, "answer");
+		
+		return answerVOs;
+	}
+
+	@Override
+	public void addOrEditAnswer(AnswerVO answerVO) {
+		
+		Answer answer = DozerUtil.xmlConfig().map(answerVO, Answer.class, "answer");
+		
+		this.answerDao.addOrEditAnswer(answer);		
+	}
 
 }
