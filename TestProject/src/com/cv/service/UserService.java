a2f3273b7a0d0802d4acaae80c8e500bdb9012ab
yogesh.zagade@creativevirtual.com <yogesh.zagade@creativevirtual.com@68a613f8-3bbb-5ddf-70fa-e19565136997>
2013-11-13 05:12:52 +0000

    New changes

--- TestProject/src/com/cv/service/UserService.java
+++ TestProject/src/com/cv/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.cv.model.Password;
 import com.cv.model.User;
+import com.cv.vo.UserVO;
 
 public interface UserService {
 
@@ -12,5 +13,7 @@ public interface UserService {
 	public void updatePassword(User user, Password password);
 
 	public List<String> getRolesForUserId(String roleId);
+
+	public UserVO getUserDetails(String username, String password);
 
 }
