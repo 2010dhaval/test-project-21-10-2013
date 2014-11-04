13bebddfab99fabe957e753ffb7a609e0a3f4f86
yogesh.zagade@creativevirtual.com <yogesh.zagade@creativevirtual.com@68a613f8-3bbb-5ddf-70fa-e19565136997>
2014-11-04 12:30:02 +0000

    test project updated on 4 nov 2014

---
+++ TestProject/src/com/cv/utils/StringOccurrenceVO.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.cv.utils;
+
+/**
+ * @author pradeep9517
+ */
+public class StringOccurrenceVO
+{
+
+	private int	totalCount;
+	private int	startindex;
+	private int	endIndex;
+
+	/**
+	 * @return the totalCount
+	 */
+	public int getTotalCount()
+	{
+		return totalCount;
+	}
+
+	/**
+	 * @param totalCount the totalCount to set
+	 */
+	public void setTotalCount(int totalCount)
+	{
+		this.totalCount = totalCount;
+	}
+
+	/**
+	 * @return the startindex
+	 */
+	public int getStartindex()
+	{
+		return startindex;
+	}
+
+	/**
+	 * @param startindex the startindex to set
+	 */
+	public void setStartindex(int startindex)
+	{
+		this.startindex = startindex;
+	}
+
+	/**
+	 * @return the endIndex
+	 */
+	public int getEndIndex()
+	{
+		return endIndex;
+	}
+
+	/**
+	 * @param endIndex the endIndex to set
+	 */
+	public void setEndIndex(int endIndex)
+	{
+		this.endIndex = endIndex;
+	}
+
+}
